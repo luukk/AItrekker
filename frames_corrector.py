7c0ff85db5b30365ae0df1aeff12a4ae0f5db13c
Alan <alanjzhong@gmail.com>
2021-10-19 14:59:04 +0200

    Add frame interpolation functionality with separate module

--- frames_corrector.py
+++ frames_corrector.py
@@ -1,3 +1,7 @@
+import math
+import re
+
+import numpy as np
 import pandas as pd
 from absl import app, flags, logging
 from absl.flags import FLAGS
@@ -9,19 +13,24 @@ flags.DEFINE_string('output', None, 'Data file to write to; e.g. \'.csv\'')
 def main(_argv):
     df = pd.read_csv(f"{FLAGS.input}")
 
-    p1, p2 = get_player_positions(df)
-    # test = p1['x'].interpolate(method='linear')
-    # test2 = p2['x'].interpolate(method='linear')
-    test2 = interpolate_missing_frames(p2)
+    players = write_filled_frames(df)
 
-    if FLAGS.output:
-        p2.to_csv('./output/corrected/nan.csv')
-        # for player in player_positions:
-        test2.to_csv(f"{FLAGS.output}")
+    input_file = re.findall(r'[^\/]+(?=\.)', FLAGS.input)[0]
+    players = [
+        pd.read_csv(f"./output/frame_corrected/{input_file}_player_1.csv"),
+        pd.read_csv(f"./output/frame_corrected/{input_file}_player_2.csv")
+    ]
 
+    for player in players:
+        player['x'].interpolate(method='index', inplace=True)
+        player['y'].interpolate(method='index', inplace=True)
 
-def interpolate_missing_frames(df):
-    breakpoint()
+    for idx, player_df in enumerate(players):
+        if FLAGS.output:
+            input_file = re.findall(r'[^\/]+(?=\.)', FLAGS.input)[0]
+            player_df.to_csv(f"{FLAGS.output}/{input_file}_player_{idx + 1}.csv", index=False)
+        else:
+            player_df.to_csv(f"player_{idx + 1}.csv", index=False)
 
 
 def fill_missing_frames(df, first_frame, max_frames, pid):
@@ -47,17 +56,21 @@ def fill_missing_frames(df, first_frame, max_frames, pid):
     return pd.DataFrame.from_dict(dcf).transpose()
 
 
-def get_player_positions(df):
+def write_filled_frames(df):
     p1 = df[df['id'] == '1']
     p2 = df[df['id'] == '2']
     p1_dict = p1.to_dict(orient='index')
     p2_dict = p2.to_dict(orient='index')
 
     max_frames = max(p1['frame'].iloc[-1], p2['frame'].iloc[-1])
-    p1 = fill_missing_frames(p1_dict, p1['frame'][0], max_frames, '1')
-    p2 = fill_missing_frames(p2_dict, p2['frame'][1], max_frames, '2')
+    players = [fill_missing_frames(p1_dict, p1['frame'][0], max_frames, '1'), fill_missing_frames(p2_dict, p2['frame'][1], max_frames, '2')]
 
-    return [p1, p2]
+    for idx, player_df in enumerate(players):
+        if FLAGS.output:
+            input_file = re.findall(r'[^\/]+(?=\.)', FLAGS.input)[0]
+            player_df.to_csv(f"./output/frame_corrected/{input_file}_player_{idx + 1}.csv", index=False)
+        else:
+            player_df.to_csv(f"player_{idx + 1}.csv", index=False)
 
 
 if __name__ == '__main__':
