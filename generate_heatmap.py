fcbd50b94fff988e793e0e1ffd9fbad8af2f4221
Alan <alanjzhong@gmail.com>
2021-11-10 11:51:18 +0100

    Remove unused package import

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -7,7 +7,6 @@ from os.path import isfile, join
 from pathlib import Path
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from absl import app, flags, logging
